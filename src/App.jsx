cfb70255695a09eea527039f7f76c17c13fd8d8e
Ali Ashooriyoon <aliashooriyoon@yahoo.com>
2025-02-21 21:54:01 +0330

    fix some Problems

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,22 @@ const App = () => {
   const [projects, setProjects] = useState([
     {
       title: "Willkommen",
-      descriptiopn: "Willkommen bei unserer Webseite!",
+      description:
+        "Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses, wobei die ersten Menschen heute nicht sind. Zudem sind Beispiele jetzt noch frei, denn man sollte bedenken, der Text macht wenig Sinn. Mit brausender Geschwindigkeit war der Titel des neuen Boots in naher Ferne, weshalb sich der Briefträger einen roten Mantel zur Reinigung brachte. Ein weiterer Punkt wäre es, die verständliche Blindtexte heute noch anzurufen denn der Tisch steht im Gang. Zur damaligen Zeit war es eher !",
       tasks: [
-        "wie kann man diese App verwenden?",
-        "wie kann man diese App verwenden2?",
+        { task: "wie kann man diese App verwenden?", id: 1 },
+        { task: "Welche Probleme hat diese App?", id: 1 },
       ],
       id: 1,
     },
     {
       title: "Willkommen2",
-      descriptiopn: "Willkommen bei unserer Webseite!",
-      tasks: ["wie kann man diese App verwenden?"],
+      description:
+        "Die ist ein Beispiel für einen Blindtext, der auf deutsch geschrieben ist. Es gibt noch viele weitere Arten des Hauses, wobei die ersten Menschen heute nicht sind. Zudem sind Beispiele jetzt noch frei, denn man sollte bedenken, der Text macht wenig Sinn. Mit brausender Geschwindigkeit war der Titel des neuen Boots in naher Ferne, weshalb sich der Briefträger einen roten Mantel zur Reinigung brachte. Ein weiterer Punkt wäre es, die verständliche Blindtexte heute noch anzurufen denn der Tisch steht im Gang. Zur damaligen Zeit war es eher üblich, die gebrauchten Handtücher mittels Fensterreiniger auszulüften, um auch die Wanduhr als Beispiel",
+      tasks: [
+        { task: "ist ein Beispiel für einen Blindtext", id: 1 },
+        { task: "der auf deutsch geschrieben ist", id: 1 },
+      ],
       id: 2,
     },
   ]);
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -13,17 +13,37 @@ const Home = ({ projects, statusProject }) => {
             </span>
           </h2>
         ) : (
-          <div className="text-center text-6xl pt-[19rem]">
+          <div className="pl-3 text-left text-6xl pt-[1rem]">
             {projects.map((item) => {
               if (item.id == statusProject) {
                 return (
-                  <div className="projectBox">
+                  <div className="projectBox flex flex-col gap-6">
                     <p className="title">{item.title}</p>
-                    <p className="description">{item.descriptiopn}</p>
-                    <p className="title">
-                      {item.tasks.map((i) => (
-                        <p>{i}</p>
-                      ))}
+                    <button className="absolute right-8 text-xl text-center border-2 w-32 outline-0 border-white cursor-pointer p-1 bg-red-500 rounded-2xl ">
+                      Löschen
+                    </button>
+
+                    <p className="description text-xl max-w-[60rem]">
+                      {item.description}
+                    </p>
+                    <p className="tasksBox text-xl">
+                      <h3 className="text-4xl text-cyan-700">Aufgaben : </h3>
+                      <div className="tasksManager  text-white w-[44rem] flex flex-col gap-2 pl-7">
+                        {item.tasks.map((i) => (
+                          <div
+                            className="my-5 bg-zinc-700 p-3 flex justify-between"
+                            key={i.id}
+                          >
+                            {i.task}{" "}
+                            <button
+                              className="text-black cursor-pointer"
+                              type="button"
+                            >
+                              Löschen
+                            </button>{" "}
+                          </div>
+                        ))}
+                      </div>
                     </p>
                   </div>
                 );
